83ed8ab2527d1efa7665c19d6feada2b9fb5b667
David Nguyen <134326617+dangnyen@users.noreply.github.com>
2024-08-15 22:54:56 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+// Koi Animation
+// $.keyframe.define({
+//     name: "swim",
+//     from: {
+//         "transform": "rotate(0deg)"
+//     },
+//     to: {
+//         "transform": "rotate(-360deg)"
+//     }
+// });
+
+// $(document).ready(function(){
+//     $(".naviButton").hover(function() {
+//         $(".koi").playKeyframe({
+//             name: "swim",
+//             duration: "5s", 
+//             timingFunction: "ease-in-out", 
+//             delay: "1s", 
+//             iterationCount: "infinite"
+//         })
+//     })
+// })
+
+
 // Accessibility Theme Switch
 
 // Contact Form Input
